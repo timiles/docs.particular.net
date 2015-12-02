40840cd9fe3b71cfae1975436a57134aaeadaf58
Tim Bussmann <tim.bussmann@particular.net>
2015-12-02 16:36:40 +0100

    updated pipeline customization page

---
+++ Snippets/Snippets_6/Pipeline/SharingBehaviorData.cs
@@ -0,0 +1,38 @@
+﻿namespace Snippets6.Pipeline
+{
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Pipeline;
+    using NServiceBus.Pipeline.Contexts;
+
+#region SharingBehaviorData
+
+    public class ParentBehavior : Behavior<PhysicalMessageProcessingContext>
+    {
+        public override async Task Invoke(PhysicalMessageProcessingContext context, Func<Task> next)
+        {
+            // set some shared information on the context
+            context.Set(new SharedData());
+
+            await next();
+        }
+    }
+
+    public class ChildBehavior : Behavior<LogicalMessageProcessingContext>
+    {
+        public override async Task Invoke(LogicalMessageProcessingContext context, Func<Task> next)
+        {
+            // access the shared data
+            SharedData data = context.Get<SharedData>();
+
+            await next();
+        }
+    }
+
+#endregion
+
+    public class SharedData
+    {
+    }
+}
